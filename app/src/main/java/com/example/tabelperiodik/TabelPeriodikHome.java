046d0f4f6747950eeca26c65126f4d9cf5ddf0d0
iwayankurniawan <kurniawanadityawardana@gmail.com>
2017-09-06 11:33:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/tabelperiodik/TabelPeriodikHome.java
@@ -0,0 +1,13 @@
+package com.example.tabelperiodik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TabelPeriodikHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tabel_periodik_home);
+    }
+}
