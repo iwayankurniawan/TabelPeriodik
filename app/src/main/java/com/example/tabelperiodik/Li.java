1d85da1586d41db22ab43588a004ba23060d3e0f
chardbud <richard_budianto@yahoo.com>
2017-09-11 21:30:14 +0200

    Coba di tes fitur pop up masing2 atomnya apakah benar seperti itu?

---
+++ app/src/main/java/com/example/tabelperiodik/Li.java
@@ -0,0 +1,27 @@
+package com.example.tabelperiodik;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by Richard on 11/09/2017.
+ */
+
+public class Li extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pop_up_li);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.72),(int)(height*.95));
+    }
+}
--- app/src/main/java/com/example/tabelperiodik/TabelPeriodikHome.java
+++ app/src/main/java/com/example/tabelperiodik/TabelPeriodikHome.java
@@ -1,7 +1,10 @@
 package com.example.tabelperiodik;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class TabelPeriodikHome extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class TabelPeriodikHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabel_periodik_home);
+
+        ImageButton h = (ImageButton) findViewById(R.id.H);
+        ImageButton li = (ImageButton) findViewById(R.id.Li);
+
+        h.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+             startActivity(new Intent(TabelPeriodikHome.this,H.class));
+
+            }
+        });
+        li.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(TabelPeriodikHome.this,Li.class));
+
+            }
+        });
     }
 }
